bb055c485128bc7b403f379ffeaeeca460ac41eb
QuietSup <feeling_of_infinity@outlook.com>
2026-03-15 16:00:52 +0100

    init

---
+++ src/test/java/com/example/fx_rate/FxRateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fx_rate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
